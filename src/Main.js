95dc498888d8461e19552315ad8ec602f08a80d5
Mike Copanos <mcopanos@gmail.com>
2019-05-22 23:07:11 -0500

    completed nav bar

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import NavBar from './components/NavBar';
 
 function Main() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <NavBar/>
+
     </div>
   );
 }
