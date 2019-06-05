6e0bedd9b11e494abc81730a479340e686eb3f30
Mike Copanos <mcopanos@gmail.com>
2019-06-04 22:49:48 -0500

    added team photos

--- src/Main.js
+++ src/Main.js
@@ -4,15 +4,17 @@ import HomeHero from './components/HomeHero';
 import SecondaryHero from './components/SecondaryHero';
 import ExpertTeam from './components/ExpertTeam';
 
-function Main() {
-  return (
-    <div>
-      <NavBar/>
-      <HomeHero/>
-      <SecondaryHero/>
-      <ExpertTeam/>
-    </div>
-  );
+class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                <NavBar/>
+                <HomeHero/>
+                <SecondaryHero/>
+                <ExpertTeam/>
+            </div>
+        );
+    }
 }
 
 export default Main;
--- src/components/ExpertTeam.js
+++ src/components/ExpertTeam.js
@@ -1,36 +1,110 @@
 import React from 'react';
 
-function ExpertTeam() {
-    return(
-      <section>
-          <div className="expert-content">
-              <h2>Our Expert Team</h2>
-              <br/>
-              <p>Lorem ipsum dolor sit amet,
-                  consectetur adipiscing elit,
-                  sed do eiusmod tempor incididunt
-                  ut labore et dolore magna aliqua.
-                  Vel facilisis volutpat est velit.
-                  Lectus sit amet est placerat in.
-                  Senectus et netus et malesuada fames
-                  ac turpis egestas integer. Nec feugiat
-                  nisl pretium fusce.</p>
-          </div>
-          <div className="image-container">
-              <div className="box box1"></div>
-              <div className="box box2"></div>
-              <div className="box box3"></div>
-              <div className="box box4"></div>
-              <div className="box box5"></div>
-              <div className="box box6"></div>
-              <div className="box box7"></div>
-              <div className="box box8"></div>
-              <div className="box box9"></div>
-              <div className="box box10"></div>
-              <div className="box box11"></div>
-              <div className="box box12"></div>
-          </div>
-      </section>
-    );
+class ExpertTeam extends React.Component {
+    render() {
+        return(
+            <section>
+                <div className="expert-content">
+                    <h2>Our Expert Team</h2>
+                    <br/>
+                    <p>Lorem ipsum dolor sit amet,
+                        consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt
+                        ut labore et dolore magna aliqua.
+                        Vel facilisis volutpat est velit.
+                        Lectus sit amet est placerat in.
+                        Senectus et netus et malesuada fames
+                        ac turpis egestas integer. Nec feugiat
+                        nisl pretium fusce.</p>
+                </div>
+                <div className="image-container">
+                    <div className="box">
+                        <img className="random-img" src="https://picsum.photos/400?random=1" alt=""/>
+                        <div className="img-content">
+                            <h2>Random Photo</h2><br/>
+                            <p>This Pic is dope!</p>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img className="random-img" src="https://picsum.photos/400?random=2" alt=""/>
+                        <div className="img-content">
+                            <h2>Random Photo</h2><br/>
+                            <p>This Pic is dope!</p>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img className="random-img" src="https://picsum.photos/400?random=3" alt=""/>
+                        <div className="img-content">
+                            <h2>Random Photo</h2><br/>
+                            <p>This Pic is dope!</p>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img className="random-img" src="https://picsum.photos/400?random=4" alt=""/>
+                        <div className="img-content">
+                            <h2>Random Photo</h2><br/>
+                            <p>This Pic is dope!</p>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img className="random-img" src="https://picsum.photos/400?random=5" alt=""/>
+                        <div className="img-content">
+                            <h2>Random Photo</h2><br/>
+                            <p>This Pic is dope!</p>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img className="random-img" src="https://picsum.photos/400?random=6" alt=""/>
+                        <div className="img-content">
+                            <h2>Random Photo</h2><br/>
+                            <p>This Pic is dope!</p>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img className="random-img" src="https://picsum.photos/400?random=7" alt=""/>
+                        <div className="img-content">
+                            <h2>Random Photo</h2><br/>
+                            <p>This Pic is dope!</p>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img className="random-img" src="https://picsum.photos/400?random=8" alt=""/>
+                        <div className="img-content">
+                            <h2>Random Photo</h2><br/>
+                            <p>This Pic is dope!</p>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img className="random-img" src="https://picsum.photos/400?random=9" alt=""/>
+                        <div className="img-content">
+                            <h2>Random Photo</h2><br/>
+                            <p>This Pic is dope!</p>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img className="random-img" src="https://picsum.photos/400?random=10" alt=""/>
+                        <div className="img-content">
+                            <h2>Random Photo</h2><br/>
+                            <p>This Pic is dope!</p>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img className="random-img" src="https://picsum.photos/400?random=11" alt=""/>
+                        <div className="img-content">
+                            <h2>Random Photo</h2><br/>
+                            <p>This Pic is dope!</p>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img className="random-img" src="https://picsum.photos/400?random=12" alt=""/>
+                        <div className="img-content">
+                            <h2>Random Photo</h2><br/>
+                            <p>This Pic is dope!</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
 }
 export default ExpertTeam;
