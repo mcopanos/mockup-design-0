4c6cc23c13311abb670d70ea1457c374584d9ed5
Mike Copanos <mcopanos@gmail.com>
2019-06-16 14:16:05 -0500

    padding and sizing fixes

--- src/components/ExpertTeam.js
+++ src/components/ExpertTeam.js
@@ -5,101 +5,109 @@ class ExpertTeam extends React.Component {
         return(
             <section>
                 <div className="expert-content">
-                    <h2>Our Expert Team</h2>
-                    <br/>
-                    <p>Lorem ipsum dolor sit amet,
-                        consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt
-                        ut labore et dolore magna aliqua.
-                        Vel facilisis volutpat est velit.
-                        Lectus sit amet est placerat in.
-                        Senectus et netus et malesuada fames
-                        ac turpis egestas integer. Nec feugiat
-                        nisl pretium fusce.</p>
+                    <h2>Our Expert Team</h2><br/>
+                    <p>
+                        The Mostly Serious team is the product
+                        of our commitment to finding those who
+                        we affectionately call <em>unicorn people</em>-the
+                        rare folks who wield an expert knowledge of their own
+                        domain and live by our core values of accountability,
+                        integrity, community, devotion, collaboration, and grit.
+                        And sure, maybe they have other random talents, like a
+                        willingness to literally run around the office delivering
+                        high five for every big victory. Overall, though, it's our
+                        collective dedication to doing the right thing in the best
+                        way that makes us a creative force ot be reckoned with.
+                    </p>
                 </div>
                 <div className="image-container">
                     <div className="box">
-                        <img className="random-img" src="https://picsum.photos/400?random=1" alt=""/>
+                        <img className="random-img" src="https://www.mostlyserious.io/uploads/team/_tile/digital-solutions-jarad.jpg" alt=""/>
                         <div className="img-content">
-                            <h2>Random Photo</h2><br/>
-                            <p>This Pic is dope!</p>
+                            <h2>Jarad Johnson</h2>
+                            <p>President</p>
                         </div>
                     </div>
                     <div className="box">
-                        <img className="random-img" src="https://picsum.photos/400?random=2" alt=""/>
+                        <img className="random-img" src="https://www.mostlyserious.io/uploads/team/_tile/digital-solutions-lucy-2.jpg" alt=""/>
                         <div className="img-content">
-                            <h2>Random Photo</h2><br/>
-                            <p>This Pic is dope!</p>
+                            <h2>Lucy Howell</h2>
+                            <p>Director of Client Strategy</p>
                         </div>
                     </div>
                     <div className="box">
-                        <img className="random-img" src="https://picsum.photos/400?random=3" alt=""/>
+                        <img className="random-img" src="https://www.mostlyserious.io/uploads/team/_tile/digital-solutions-spencer.jpg" alt=""/>
                         <div className="img-content">
-                            <h2>Random Photo</h2><br/>
-                            <p>This Pic is dope!</p>
+                            <h2>Spencer Harris</h2>
+                            <p>director of Operations</p>
                         </div>
                     </div>
                     <div className="box">
-                        <img className="random-img" src="https://picsum.photos/400?random=4" alt=""/>
+                        <img className="random-img" src="https://www.mostlyserious.io/uploads/team/_tile/digital-solutions-chelsea.jpg" alt=""/>
                         <div className="img-content">
-                            <h2>Random Photo</h2><br/>
-                            <p>This Pic is dope!</p>
+                            <h2>Chelsea Begley</h2>
+                            <p>Account Manger</p>
                         </div>
                     </div>
                     <div className="box">
-                        <img className="random-img" src="https://picsum.photos/400?random=5" alt=""/>
+                        <img className="random-img" src="https://www.mostlyserious.io/uploads/team/_tile/digital-solutions-jess.jpg" alt=""/>
                         <div className="img-content">
-                            <h2>Random Photo</h2><br/>
-                            <p>This Pic is dope!</p>
+                            <h2>Jessica Spencer</h2>
+                            <p>Director of Design</p>
                         </div>
                     </div>
                     <div className="box">
-                        <img className="random-img" src="https://picsum.photos/400?random=6" alt=""/>
+                        <img className="random-img" src="https://www.mostlyserious.io/uploads/team/_tile/digital-solutions-molly.jpg" alt=""/>
                         <div className="img-content">
-                            <h2>Random Photo</h2><br/>
-                            <p>This Pic is dope!</p>
+                            <h2>Molly Riddle-Nunn</h2>
+                            <p>Director of Content Strategy</p>
                         </div>
                     </div>
                     <div className="box">
-                        <img className="random-img" src="https://picsum.photos/400?random=7" alt=""/>
+                        <img className="random-img" src="https://www.mostlyserious.io/uploads/team/_tile/digital-solutions-maranda.jpg" alt=""/>
                         <div className="img-content">
-                            <h2>Random Photo</h2><br/>
-                            <p>This Pic is dope!</p>
+                            <h2>Maranda Provance</h2>
+                            <p>Director of Engineering</p>
                         </div>
                     </div>
                     <div className="box">
-                        <img className="random-img" src="https://picsum.photos/400?random=8" alt=""/>
+                        <img className="random-img" src="https://www.mostlyserious.io/uploads/team/_tile/digital-solutions-aimee.jpg" alt=""/>
                         <div className="img-content">
-                            <h2>Random Photo</h2><br/>
-                            <p>This Pic is dope!</p>
+                            <h2>Aimee Hendrycks</h2>
+                            <p>Senior Engineer </p>
                         </div>
                     </div>
                     <div className="box">
-                        <img className="random-img" src="https://picsum.photos/400?random=9" alt=""/>
+                        <img className="random-img" src="https://www.mostlyserious.io/uploads/team/_tile/digital-solutions-jt.jpg" alt=""/>
                         <div className="img-content">
-                            <h2>Random Photo</h2><br/>
-                            <p>This Pic is dope!</p>
+                            <h2>Jessica Gottstein</h2>
+                            <p>Designer</p>
                         </div>
                     </div>
                     <div className="box">
-                        <img className="random-img" src="https://picsum.photos/400?random=10" alt=""/>
+                        <img className="random-img" src="https://www.mostlyserious.io/uploads/team/_tile/digital-solutions-becca.jpg" alt=""/>
                         <div className="img-content">
-                            <h2>Random Photo</h2><br/>
-                            <p>This Pic is dope!</p>
+                            <h2>Becca Godsey</h2>
+                            <p>Marketing Analyst</p>
                         </div>
                     </div>
                     <div className="box">
-                        <img className="random-img" src="https://picsum.photos/400?random=11" alt=""/>
+                        <img className="random-img" src="https://www.mostlyserious.io/uploads/team/_tile/digital-solutions-levi.jpg" alt=""/>
                         <div className="img-content">
-                            <h2>Random Photo</h2><br/>
-                            <p>This Pic is dope!</p>
+                            <h2>Levi Zitting</h2>
+                            <p>Engineer</p>
                         </div>
                     </div>
                     <div className="box">
-                        <img className="random-img" src="https://picsum.photos/400?random=12" alt=""/>
+                        <img className="random-img" src="https://www.mostlyserious.io/uploads/blog/_tile/MOS_Joe-Camden-Who-We-Are.jpg" alt=""/>
                         <div className="img-content">
-                            <h2>Random Photo</h2><br/>
-                            <p>This Pic is dope!</p>
+                            <h2>Joe Camden</h2>
+                            <p>Video and Motion Producer</p>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img className="random-img" src="https://picsum.photos/400?random=13" alt=""/>
+                        <div className="img-content">
                         </div>
                     </div>
                 </div>
