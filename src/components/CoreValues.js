ea024e70472f0774c24ff487feba55fdbf43e631
Mike Copanos <mcopanos@gmail.com>
2019-06-24 21:58:27 -0500

    content and styles for Our Core Values

--- src/components/CoreValues.js
+++ src/components/CoreValues.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 function CoreValues() {
     return (
-        <section>
+        <section className="core-values">
+            <div className="core-values-content">
+                <hr className="core-values-line"/>
+                <h2>Our Core Values</h2>
+                <p>
+                    Our core values are essentially our own
+                    North Stars. They're the root cause of
+                    our wins and what we look to for guidance
+                    when we're challenged the most. They push
+                    us forward and upward, lead us safely away
+                    from missteps, and reminds us why we dou what
+                    we do. We think every team should stand for
+                    something-why not six things?
+                </p>
+            </div>
 
         </section>
     );
