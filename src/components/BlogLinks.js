d3c0e7239bf179eac4330cf6cc939fa2662b7527
Michael Copanos <michaelcopanos@Michaels-MBP.attlocal.net>
2019-07-10 20:03:03 -0500

    clean working branch for a git push

--- src/components/BlogLinks.js
+++ src/components/BlogLinks.js
@@ -9,23 +9,25 @@ const BlogLinks = () => (
             </div>
             <div className="blog-set__flex-container">
                 <div className="blog-set__blog-posts">
-                    {blogData.map((blogPost, index) => (
-                        <div key={index} className="article-card">
-                            <div className="article-card__image-wrapper">
-                                <img className="article-card__image" src={blogPost.image} alt=""/>
-                            </div>
-                            <h5>{blogPost.title}</h5>
-                            <div dangerouslySetInnerHTML={{__html: blogPost.body}}/>
-                            <div>
-                                <div className="article-card__image">
-                                    <img src={blogPost.author.image} alt=""/>
+                    {
+                        blogData.map((blogPost, index) => (
+                            <div key={index} className="article-card">
+                                <div className="article-card__image-wrapper">
+                                    <img className="article-card__image" src={blogPost.image} alt=""/>
                                 </div>
-                                <div className="cite-author">
-                                    <p>{blogPost.author.name}</p>
+                                <h5>{blogPost.title}</h5>
+                                <div dangerouslySetInnerHTML={{__html: blogPost.body}}/>
+                                <div>
+                                    <div className="article-card__image">
+                                        <img src={blogPost.author.image} alt=""/>
+                                    </div>
+                                    <div className="cite-author">
+                                        <p>{blogPost.author.name}</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        ))
+                    }
                 </div>
             </div>
         </div>
