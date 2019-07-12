0955d1d17b237b5916ed9d6e8b0d493a4b204868
Mike Copanos <mcopanos@gmail.com>
2019-07-11 20:39:41 -0500

    clean working branch

--- src/components/BlogLinks.js
+++ src/components/BlogLinks.js
@@ -3,32 +3,30 @@ import blogData from '../data/blogPosts.json'
 
 const BlogLinks = () => (
     <section className="blog-set">
-        <div className="blog-container">
-            <div className="centered-content">
-                <h2>What We've Been Writing</h2>
-            </div>
-            <div className="blog-set__flex-container">
-                <div className="blog-set__blog-posts">
-                    {
-                        blogData.map((blogPost, index) => (
-                            <div key={index} className="article-card">
-                                <div className="article-card__image-wrapper">
-                                    <img className="article-card__image" src={blogPost.image} alt=""/>
+        <div className="centered-content">
+            <h2>What We've Been Writing</h2>
+        </div>
+        <div className="container">
+            <div className="blog-set__blog-posts">
+                {
+                    blogData.map(blogPost => (
+                        <div key={blogPost.id} className="article-card">
+                            <div className="article-card__image-wrapper">
+                                <img className="article-card__image" src={blogPost.image} alt=""/>
+                            </div>
+                            <h5>{blogPost.title}</h5>
+                            <div dangerouslySetInnerHTML={{__html: blogPost.body}}/>
+                            <div>
+                                <div className="article-card__image">
+                                    <img src={blogPost.author.image} alt=""/>
                                 </div>
-                                <h5>{blogPost.title}</h5>
-                                <div dangerouslySetInnerHTML={{__html: blogPost.body}}/>
-                                <div>
-                                    <div className="article-card__image">
-                                        <img src={blogPost.author.image} alt=""/>
-                                    </div>
-                                    <div className="cite-author">
-                                        <p>{blogPost.author.name}</p>
-                                    </div>
+                                <div className="cite-author">
+                                    <p>{blogPost.author.name}</p>
                                 </div>
                             </div>
-                        ))
-                    }
-                </div>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     </section>
